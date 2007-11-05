39c354aad4dc75aec86f1b514a1815c3ff2d8c92
Keith Beattie <ksbeattie@lbl.gov>
2007-11-05 11:54:35 -0600

    Issue #1932: Fix "Bogus df line "/dev/mapper/VolGroup00-LogVol00" error
    By adding a -P (posix compat) flag to the df command.
    
    Note that on BSD-based systems (like Mac OSX) the order of the -k and
    -P flags are important.

--- src/main/java/icecube/daq/juggler/mbean/SystemStatistics.java
+++ src/main/java/icecube/daq/juggler/mbean/SystemStatistics.java
@@ -32,8 +32,9 @@ public class SystemStatistics
         Pattern.compile("^.*load\\s+averages?:" + LOAD_AVG_STR + LOAD_AVG_STR +
                         LOAD_AVG_STR);
 
-    /** disk free space command. */
-    private ProcessBuilder df = new ProcessBuilder("df", "-l", "-k");
+    /** disk free space command. The -k must be *after* the -P on
+     * BSD-based systems! */
+    private ProcessBuilder df = new ProcessBuilder("df", "-P", "-l", "-k");
     /** pattern for parsing 'df' output. */
     private Pattern dfPat =
         Pattern.compile("^\\s*(\\S+)\\s+(\\d+)\\s+(\\d+)\\s+(\\d+)" +
