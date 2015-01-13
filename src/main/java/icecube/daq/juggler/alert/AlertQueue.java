3bab3ee88cdd1d8b93c03a9b67d87bdec792eb5d
Dave Glowacki <dglo@icecube.wisc.edu>
2015-01-13 06:59:54 -0600

    Issue 7201: Make stopAndWait() more reliable; add javadoc

--- src/main/java/icecube/daq/juggler/alert/AlertQueue.java
+++ src/main/java/icecube/daq/juggler/alert/AlertQueue.java
@@ -37,11 +37,23 @@ public class AlertQueue
     private boolean stopping;
     private boolean stopped = true;
 
+    /**
+     * Create an AlertQueue with a thread named <tt>"AlertQueue"</tt>
+     *
+     * @param alerter source of alerts
+     */
     public AlertQueue(Alerter alerter)
     {
         this("AlertQueue", alerter);
     }
 
+    /**
+     * Create a named AlertQueue
+     *
+     * @param name thread name
+     *
+     * @param alerter source of alerts
+     */
     public AlertQueue(String name, Alerter alerter)
     {
         this.name = name;
@@ -96,31 +108,64 @@ public class AlertQueue
         return map;
     }
 
+    /**
+     * Get number of alerts dropped while the queue was full
+     *
+     * @return number of dropped alerts
+     */
     public long getNumDropped()
     {
         return numDropped;
     }
 
+    /**
+     * Get number of alerts queued for delivery
+     *
+     * @return number of queued alerts
+     */
     public int getNumQueued()
     {
         return queue.size();
     }
 
+    /**
+     * Get number of alerts successfully sent
+     *
+     * @return number of sent alerts
+     */
     public long getNumSent()
     {
         return numSent;
     }
 
+    /**
+     * Is the thread idle?
+     *
+     * @return <tt>true</tt> if the thread is waiting for alerts
+     */
     public boolean isIdle()
     {
         return idle;
     }
 
+    /**
+     * Is the thread stopped?
+     *
+     * @return <tt>true</tt> if the thread is not running
+     */
     public boolean isStopped()
     {
         return stopped;
     }
 
+    /**
+     * Add <tt>obj</tt> to the queue of alerts to be sent
+     *
+     * @param obj alert to be sent
+     *
+     * @throws AlertException if there is a problem with the alerter or
+     *                        the thread is stopped
+     */
     public void push(Object obj)
         throws AlertException
     {
@@ -157,6 +202,16 @@ public class AlertQueue
         }
     }
 
+    /**
+     * Add this alert to the queue of alerts to be sent
+     *
+     * @param varname alert name
+     * @param prio alert priority
+     * @param values alert values
+     *
+     * @throws AlertException if there is a problem with the alerter or
+     *                        the thread is stopped
+     */
     public void push(String varname, Alerter.Priority prio,
                      Map<String, Object> values)
         throws AlertException
@@ -164,6 +219,17 @@ public class AlertQueue
         push(buildMessage(varname, prio, values));
     }
 
+    /**
+     * Add this alert to the queue of alerts to be sent
+     *
+     * @param varname alert name
+     * @param prio alert priority
+     * @param utcTime alert time
+     * @param values alert values
+     *
+     * @throws AlertException if there is a problem with the alerter or
+     *                        the thread is stopped
+     */
     public void push(String varname, Alerter.Priority prio, IUTCTime utcTime,
                      Map<String, Object> values)
         throws AlertException
@@ -171,6 +237,9 @@ public class AlertQueue
         push(buildMessage(varname, prio, utcTime, values));
     }
 
+    /**
+     * Run the thread
+     */
     public void run()
     {
         stopped = false;
@@ -181,6 +250,9 @@ public class AlertQueue
         }
     }
 
+    /**
+     * Main thread loop
+     */
     private void runLoop()
     {
         while (!stopping || queue.size() > 0) {
@@ -224,6 +296,13 @@ public class AlertQueue
         thread = null;
     }
 
+    /**
+     * Set the object responsible for sending alerts
+     *
+     * @param alerter alerter object
+     *
+     * @throws Error if alerter is null
+     */
     public void setAlerter(Alerter alerter)
     {
         if (alerter == null) {
@@ -233,11 +312,20 @@ public class AlertQueue
         this.alerter = alerter;
     }
 
+    /**
+     * Set the maximum queue size.
+     * NOTE: This is only used by unit tests
+     *
+     * @param size maximum queue size
+     */
     public void setMaxQueueSize(int size)
     {
         maxQueueSize = size;
     }
 
+    /**
+     * Start the thread if it isn't already running
+     */
     public void start()
     {
         synchronized (queue) {
@@ -252,6 +340,9 @@ public class AlertQueue
         }
     }
 
+    /**
+     * Stop the thread if it's running
+     */
     public void stop()
     {
         synchronized (queue) {
@@ -262,28 +353,43 @@ public class AlertQueue
         }
     }
 
+    /**
+     * Tell the thread to stop and wait until it is fully stopped.
+     * NOTE: This is currently only used by unit tests.
+     *
+     * @throws Error if the thread was not stopped
+     */
     public void stopAndWait()
     {
-        if (!stopped) {
-            stop();
-            for (int i = 0; i < 1000; i++) {
-                if (stopped) {
-                    break;
-                }
-
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException ie) {
-                    break;
-                }
-            }
-
+        synchronized (queue) {
             if (!stopped) {
-                throw new Error("Cannot stop " + name);
+                stopping = true;
+                queue.notify();
+
+                for (int i = 0; i < 1000; i++) {
+                    if (stopped) {
+                        break;
+                    }
+
+                    try {
+                        queue.wait(100);
+                    } catch (InterruptedException ie) {
+                        continue;
+                    }
+                }
             }
+        }
+
+        if (!stopped) {
+            throw new Error("Cannot stop " + name + "; queue has " +
+                            queue.size() + " alerts, stopping is " +
+                            stopping);
         }
     }
 
+    /**
+     * Thread name
+     */
     public String toString()
     {
         return name;
